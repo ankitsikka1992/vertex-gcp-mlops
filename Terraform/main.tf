50f1498d3883a6b84e220615d5528d2c5cb0ce3f
ankitsikka1992 <95908498+ankitsikka1992@users.noreply.github.com>
2024-08-08 18:33:44 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,7 @@
-# Specify the Terraform version
 terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"  # Use a specific version range, adjust as necessary
-    }
+  backend "gcs" {
+    bucket  = ""
+    prefix  = ""
   }
 }
 
